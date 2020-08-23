32aa82bf2f1100d57357ff50d3229e4aa1ac480e
Umar Farooq <farooqu475@gmail.com>
2020-08-23 21:02:16 +0500

    project initialized, also add bussinuss entities,services and centext projects

---
+++ EClothPoint.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EClothPoint.Web.MvcApplication" Language="C#" %>
---
+++ EClothPoint.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EClothPoint.Web.Startup))]
+namespace EClothPoint.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
